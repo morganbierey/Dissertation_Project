930959b459bff5991501f7f64a7b970b1c39fe8b
Morgan Bierey <bierey11@gmail.com>
2023-03-06 21:19:19 +0000

    closes #32

--- project/individual_project/populate_educational_tool.py
+++ project/individual_project/populate_educational_tool.py
@@ -4,7 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import django
 django.setup()
-from educational_tool.models import Category, Page, video
+from educational_tool.models import Category, Page, video, exercise
 
 def populate():
      # First, we will create lists of dictionaries containing the pages
@@ -14,53 +14,88 @@ def populate():
      # # through each data structure, and add the data to our models.
 
 
- video_pages = [
+        video_pages = [
         {'id':'1','title': 'Intro to why you should code',
         'url':'Dv7gLpW91DM'},
         {'id':'2','title':'What is python ',
         'url':'Y8Tko2YC5hA'},
         {'id':'3','title':'Syntax',
-         'url':'0NO3MJkxm2g'} ,
-         {'id':'4','title':'Comments',
-         'url':''},
-         {'id':'5','title':'Variables',
-         'url':'0NO3MJkxm2g'},
-         {'id':'6','title':'Data Types',
-         'url':'0NO3MJkxm2g'},
-         {'id':'7','title':'Numbers',
-         'url':'0NO3MJkxm2g'},
-         {'id':'8','title':'Type Casting',
-         'url':'0NO3MJkxm2g'},
-         {'id':'9','title':'Strings',
-         'url':'0NO3MJkxm2g'},
-         {'id':'10','title':'Booleans',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Operators',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Lists',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Tuples',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Sets',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Dictionaries',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'If/ Else',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'While Loops',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'For Loops',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Functions',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Arrays',
-         'url':'0NO3MJkxm2g'},
-         {'id':'3','title':'Scope',
          'url':'0NO3MJkxm2g'}]
+        #  {'id':'4','title':'Comments',
+        #  'url':''},
+        #  {'id':'5','title':'Variables',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'6','title':'Data Types',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'7','title':'Numbers',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'8','title':'Type Casting',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'9','title':'Strings',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'10','title':'Booleans',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Operators',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Lists',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Tuples',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Sets',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Dictionaries',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'If/ Else',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'While Loops',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'For Loops',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Functions',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Arrays',
+        #  'url':'0NO3MJkxm2g'},
+        #  {'id':'3','title':'Scope',
+        #  'url':'0NO3MJkxm2g'}
+
+        excercise_pages = [{'id':'1','title': 'Intro to why you should code',
+        'problem': """You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+            Hello firstname lastname! You just delved into python.
+
+            Function Description:
+
+            Complete the print_full_name function in the editor below.
+
+            print_full_name has the following parameters:
+
+            string first: the first name
+            string last: the last name
+            Prints
+
+            string: 'Hello  ! You just delved into python' where  and  are replaced with  and .
+            Input Format
+
+            The first line contains the first name, and the second line contains the last name.
+
+            Constraints: 
+
+            The length of the first and last names are each ≤ Sample Input 0
+
+            Ross
+            Taylor
+            Sample Output 0
+
+            Hello Ross Taylor! You just delved into python.
+            Explanation 0
+
+            The input read by the program is stored as a string data type. A string is a collection of characters."""},
+        {'id':'2','title':'What is python ',
+        'problem':'Y8Tko2YC5hA'},
+        {'id':'3','title':'Syntax',
+         'problem':'0NO3MJkxm2g'} ,]
 
 
-
- python_pages = [
+        python_pages = [
         {'title': 'Official Python Tutorial',
         'url':'http://docs.python.org/3/tutorial/'},
         {'title':'How to Think like a Computer Scientist',
@@ -68,46 +103,55 @@ def populate():
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/'} ]
     
- django_pages = [
+        django_pages = [
             {'title':'Official Django Tutorial',
-    'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-    {'title':'Django Rocks',
-    'url':'http://www.djangorocks.com/'},
-    {'title':'How to Tango with Django',
-    'url':'http://www.tangowithdjango.com/'} ]
+        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        {'title':'Django Rocks',
+        'url':'http://www.djangorocks.com/'},
+        {'title':'How to Tango with Django',
+        'url':'http://www.tangowithdjango.com/'} ]
 
- other_pages = [
-    {'title':'Bottle',
-    'url':'http://bottlepy.org/docs/dev/'},
-    {'title':'Flask',
-    'url':'http://flask.pocoo.org'} ]
+        other_pages = [
+        {'title':'Bottle',
+        'url':'http://bottlepy.org/docs/dev/'},
+        {'title':'Flask',
+        'url':'http://flask.pocoo.org'} ]
 
- cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
-     'Django': {'pages': django_pages,'views':64,'likes':32},
-     'Other Frameworks': {'pages': other_pages,'views':32,'likes':16} }
+        cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
+        'Django': {'pages': django_pages,'views':64,'likes':32},
+        'Other Frameworks': {'pages': other_pages,'views':32,'likes':16} }
 
-    # If you want to add more categories or pages,
-    # add them to the dictionaries above.
+        # If you want to add more categories or pages,
+        # add them to the dictionaries above.
 
- for video_dict in video_pages:
-        id = video_dict.get('id')
-        title = video_dict.get('title')
-        url = video_dict.get('url')
 
-        c = add_vid(id,title,url)
+
+
+        for video_dict in video_pages:
+                id = video_dict.get('id')
+                title = video_dict.get('title')
+                url = video_dict.get('url')
+                c = add_vid(id,title,url)
         
 
-    # The code below goes through the cats dictionary, then adds each category,
-    # and then adds all the associated pages for that category.
- for cat, cat_data in cats.items():
-        c = add_cat(cat,cat_data["views"],cat_data["likes"])
-        for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+        # The code below goes through the cats dictionary, then adds each category,
+        # and then adds all the associated pages for that category.
+        for cat, cat_data in cats.items():
+                c = add_cat(cat,cat_data["views"],cat_data["likes"])
+                for p in cat_data['pages']:
+                       add_page(c, p['title'], p['url'])
 
     # Print out the categories we have added.
- for c in Category.objects.all():
-        for p in Page.objects.filter(category=c):
-            print(f'- {c}: {p}')
+        for c in Category.objects.all():
+                for p in Page.objects.filter(category=c):
+                       print(f'- {c}: {p}')
+        
+        for ex_dict in excercise_pages:
+               id = ex_dict.get('id')
+               title = ex_dict.get('title')
+               problem = ex_dict.get('problem')
+               p = add_excercise(id,title,problem)
+
 
 def add_page(cat, title, url, views=0):
         p = Page.objects.get_or_create(category=cat, title=title)[0]
@@ -116,16 +160,22 @@ def add_page(cat, title, url, views=0):
         p.save()
         return p
 
+def add_vid(id,title, url):
+     v = video.objects.get_or_create(id=id,title=title,url=url)[0]
+     v.save()
+     return v
+
 def add_cat(name, views=0,likes=0):
         c = Category.objects.get_or_create(name=name,views =views, likes=likes)[0]
         c.save()
         return c
 
 
-def add_vid(id,title, url):
-     v = video.objects.get_or_create(id=id,title=title,url=url)[0]
-     v.save()
-     return v
+
+def add_excercise(id,title, problem):
+     p = exercise.objects.get_or_create(id=id,title=title,problem=problem)[0]
+     p.save()
+     return p
 
 
 # Start execution here!
